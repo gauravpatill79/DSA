4e2943ea566c9c6077c3917d55b424fea0817dd3
Gaurav Patil <108526962+gauravpatill79@users.noreply.github.com>
2026-09-07 08:26:53 +0530

    Time: 1 ms (98.63%) | Memory: 12.5 MB (64.74%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ans = []
+        depth = 0
+        for ch in s:
+            if ch == '(':
+                if depth > 0:
+                    ans.append(ch)
+                depth += 1
+            else:
+                depth -= 1
+                if depth > 0:
+                    ans.append(ch)
+        return ''.join(ans)
